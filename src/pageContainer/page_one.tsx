dbf3b35a3a8b9ce1f6313554a21cdfb4e975ef9a
DoHi0512 <donghuni512@gmail.com>
2023-03-21 20:15:08 +0900

    first page

---
+++ src/pageContainer/page_one.tsx
@@ -0,0 +1,20 @@
+export const PageOne = () => {
+  return (
+    <div className="w-full h-screen bg-bgBlue flex items-center flex-col">
+      <div className="w-full h-2/5 flex flex-col justify-center items-center">
+        <div className="text-4xl font-semibold">
+          여럿이 함께 만들어나가는 플랫폼
+        </div>
+        <div className="text-textGray mt-8">
+          사용자가 원하는 물건을 쉽게 구매 할 수 있도록<br></br>Tool은 사용자가
+          원하는 플랫폼을 제공합니다.
+        </div>
+      </div>
+      <div className="w-[70%] h-[50%] grid grid-cols-3 gap-[3rem] ">
+        <div className="bg-[white] shadow-[0px_4px_30px_rgba(0,_0,_0,_0.25)]"></div>
+        <div className="bg-[white] shadow-[0px_4px_30px_rgba(0,_0,_0,_0.25)]"></div>
+        <div className="bg-[white] shadow-[0px_4px_30px_rgba(0,_0,_0,_0.25)]"></div>
+      </div>
+    </div>
+  );
+};
