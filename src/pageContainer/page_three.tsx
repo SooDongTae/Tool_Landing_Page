8693d9a1d371568c2904a675d1f6f6eab4158e33
LLTae2 <taehyun5820@gmail.com>
2023-03-21 23:50:30 +0900

    Create::세번째 페이지 생성

---
+++ src/pageContainer/page_three.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Page_three = () => {
+  return (
+    <div className="w-screen h-fit bg-bgBlue">
+      <h1 className="text-black text-5xl">
+        쉽고 빠르게
+        <br /> 등록부터 참여까지
+        <br /> 이런 <b className="text-green">공동구매</b> 해보셨나요?
+      </h1>
+    </div>
+  );
+};
+
+export default Page_three;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Main } from "../pageContainer/main";
 import { PageOne } from "../pageContainer/page_one";
+import Page_three from "../pageContainer/page_three";
 
 export default function Home() {
   return (
     <>
       <Main />
       <PageOne />
+      <Page_three />
     </>
   );
 }
